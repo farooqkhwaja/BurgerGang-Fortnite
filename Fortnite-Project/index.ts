deb67a6b9f464d286dc3bd2f2139812f1113eb7a
Teoman Palit <s142230@ap.be>
2025-05-28 15:26:42 +0000

    changes deploy 4

--- Fortnite-Project/index.ts
+++ Fortnite-Project/index.ts
@@ -23,7 +23,7 @@ app.use(session)
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.set("views", path.join(__dirname, "..", "views"));
 
 app.set("port", process.env.PORT || 3000);
